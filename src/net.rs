565e1f70413370b880650ac8d3bdfa1abb20ccfc
Wenping Guo <ybyygu@gmail.com>
2023-04-30 14:28:55 +0800

    Use bevy tasks instead of tokio runtime

--- src/net.rs
+++ src/net.rs
@@ -2,8 +2,27 @@
 use bevy::prelude::*;
 use gut::prelude::Result;
 
-use bevy_tokio_tasks::{TokioTasksPlugin, TokioTasksRuntime};
+use gchemol_core::Molecule;
 // 495e3d25 ends here
+
+// [[file:../bevy.note::02dd4467][02dd4467]]
+use std::net::{SocketAddr, ToSocketAddrs};
+
+/// Settings to configure the network, both client and server
+#[derive(Resource, Default)]
+struct NetworkSettings {
+    address: Option<SocketAddr>,
+}
+
+#[derive(Resource)]
+struct NetworkServer {}
+
+impl NetworkServer {
+    pub(crate) fn new() -> Self {
+        todo!();
+    }
+}
+// 02dd4467 ends here
 
 // [[file:../bevy.note::bbb42a57][bbb42a57]]
 mod app_error {
@@ -33,30 +52,28 @@ mod app_error {
 // bbb42a57 ends here
 
 // [[file:../bevy.note::27477258][27477258]]
-mod handlers {
+mod routes {
     use super::app_error::AppError;
+    use gchemol_core::Molecule;
+    use gut::prelude::Result;
+
     use axum::extract::State;
     use axum::Json;
-    use gchemol_core::Molecule;
 
-    pub type TaskState = crate::task::TaskSender<Molecule, ()>;
+    pub type TaskChannelTx = crate::task::TaskSender<Molecule, ()>;
 
     #[axum::debug_handler]
-    pub(super) async fn view_molecule(State(client): State<TaskState>, Json(mol): Json<Molecule>) -> Result<(), AppError> {
+    async fn view_molecule(State(client): State<TaskChannelTx>, Json(mol): Json<Molecule>) -> Result<(), AppError> {
         let resp = client.send(mol).await?;
         Ok(())
     }
-}
-// 27477258 ends here
 
-// [[file:../bevy.note::fe9e7673][fe9e7673]]
-impl NetworkServer {
-    pub async fn serve_remote_view(task_tx: crate::task::TaskSender<gchemol_core::Molecule, ()>) -> Result<()> {
+    /// Start remote view service listening on molecules from remote client side.
+    pub async fn serve_remote_view(task_tx: TaskChannelTx) -> Result<()> {
         use axum::{routing::get, Router};
 
         use axum::routing::post;
         use gchemol_core::Molecule;
-        use handlers::view_molecule;
 
         let app = Router::new().route("/view-molecule", post(view_molecule)).with_state(task_tx);
         axum::Server::bind(&"127.0.0.1:3039".parse().unwrap())
@@ -65,95 +82,57 @@ impl NetworkServer {
 
         Ok(())
     }
-
-    pub fn handle_molecule_view_task(&mut self) -> Option<gchemol_core::Molecule> {
-        use crate::task::RemoteIO;
-
-        if let Some(rx) = &mut self.task_rx {
-            // Execute the future, blocking the current thread until completion
-            if let Some(mol) = self.runtime.block_on(async {
-                info!("hello");
-                if let Some(RemoteIO(mol, tx_out)) = rx.recv().await {
-                    // compute with job input
-                    // let output = compute_with(mol)?;
-                    // send job output to client side
-                    tx_out.send(()).ok()?;
-                    Some(mol)
-                } else {
-                    // task channel closed
-                    None
-                }
-            }) {
-                Some(mol)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }
 }
-// fe9e7673 ends here
+// 27477258 ends here
 
-// [[file:../bevy.note::bd70c1ac][bd70c1ac]]
-use std::net::{SocketAddr, ToSocketAddrs};
-
-use tokio::{
-    runtime::Runtime,
-    sync::mpsc::{channel, Receiver, Sender},
-};
+// [[file:../bevy.note::d1858fa3][d1858fa3]]
+use bevy::tasks::{IoTaskPool, Task};
+use futures_lite::future;
 
 #[derive(Resource)]
-struct NetworkServer {
-    /// tokio runtime
-    runtime: Runtime,
-    /// handle to task that listens for new connections.
-    listener_task: Option<tokio::task::JoinHandle<std::result::Result<(), bevy::asset::Error>>>,
+struct RemoteMoleculeTask(Task<Option<Molecule>>);
 
-    /// the channel for server side to get molecule from client side
-    task_rx: Option<crate::task::TaskReceiver<gchemol_core::Molecule, ()>>,
+/// Receive molecule sent from client side
+async fn recv_molecule_from(mut task_rx: crate::task::TaskReceiver<Molecule, ()>) -> Option<Molecule> {
+    use crate::task::RemoteIO;
+
+    info!("hello");
+    if let Some(RemoteIO(mol, tx_out)) = task_rx.recv().await {
+        // compute with job input
+        // let output = compute_with(mol)?;
+        // send job output to client side
+        tx_out.send(()).ok()?;
+        Some(mol)
+    } else {
+        // task channel closed
+        None
+    }
 }
 
-/// Settings to configure the network, both client and server
-#[derive(Resource, Default)]
-struct NetworkSettings {
-    address: Option<SocketAddr>,
+/// Start remote view service listening on molecules from remote client side.
+pub fn start_remote_view_service(mut commands: Commands) {
+    info!("starting remote view service ...");
+
+    let (task_rx, task_tx) = crate::task::Task::<Molecule, ()>::new().split();
+    IoTaskPool::get().spawn(routes::serve_remote_view(task_tx)).detach();
+
+    let task = IoTaskPool::get().spawn(recv_molecule_from(task_rx));
+    commands.insert_resource(RemoteMoleculeTask(task));
 }
 
-impl NetworkServer {
-    pub(crate) fn new() -> NetworkServer {
-        NetworkServer {
-            runtime: tokio::runtime::Builder::new_multi_thread()
-                .enable_all()
-                .build()
-                .expect("Could not build tokio runtime"),
-            listener_task: None,
-            task_rx: None,
+pub fn handle_remote_molecule(mut task: ResMut<RemoteMoleculeTask>) {
+    if let Some(mol_task) = future::block_on(future::poll_once(&mut task.0)) {
+        if let Some(mol) = mol_task {
+            let natoms = mol.natoms();
+            info!("get molecule: {natoms}");
+        } else {
+            debug!("mol task channel closed");
         }
-    }
-
-    pub fn listen(&mut self) -> Result<()> {
-        use gchemol_core::Molecule;
-
-        info!("Started listening for molecule from client side.");
-        let (task_rx, task_tx) = crate::task::Task::<Molecule, ()>::new().split();
-        self.task_rx = task_rx.into();
-        self.listener_task = Some(self.runtime.spawn(Self::serve_remote_view(task_tx)));
-
-        Ok(())
-    }
-
-    /// Disconnect all clients and stop listening.
-    ///
-    /// # NOTE
-    /// will do nothing if no active listening.
-    pub fn stop(&mut self) {
-        if let Some(conn) = self.listener_task.take() {
-            conn.abort();
-        }
+    } else {
+        debug!("not ready");
     }
 }
-// bd70c1ac ends here
+// d1858fa3 ends here
 
 // [[file:../bevy.note::0e0418a7][0e0418a7]]
 pub struct ServerPlugin;
@@ -161,9 +140,9 @@ pub struct ServerPlugin;
 impl Plugin for ServerPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(NetworkServer::new())
-            .insert_resource(NetworkSettings::default());
-            // .add_startup_system(start_server);
-            // .add_system(remote_view);
+            .insert_resource(NetworkSettings::default())
+            .add_startup_system(start_remote_view_service)
+            .add_system(handle_remote_molecule);
     }
 }
 // 0e0418a7 ends here
