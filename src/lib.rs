25e940c7c3d36929f87cd807a70a3c088dd90ae0
Wenping Guo <ybyygu@gmail.com>
2023-07-02 09:53:27 +0800

    Clean up

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,10 @@
 // 2fdba27f ends here
 
 // [[file:../bevy.note::3edfd207][3edfd207]]
-// mod actions;
-// mod camera;
-// mod loading;
-// mod menu;
 mod animation;
 mod base;
 mod net;
 mod ui;
-// mod task;
 
 #[cfg(not(target_arch = "wasm32"))]
 pub mod cli;
@@ -20,10 +15,6 @@ pub mod molecule;
 // 3edfd207 ends here
 
 // [[file:../bevy.note::043e6795][043e6795]]
-// use crate::actions::ActionsPlugin;
-// use crate::loading::LoadingPlugin;
-// use crate::menu::MenuPlugin;
-
 use bevy::app::App;
 use bevy::prelude::*;
 // 043e6795 ends here
